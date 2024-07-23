15e861bf9b47416df4030453cff4213b8ab023ec
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2024-07-23 18:44:53 +0300

    feat: add docker

--- src/main/java/com/application/bookstore/model/Book.java
+++ src/main/java/com/application/bookstore/model/Book.java
@@ -2,6 +2,7 @@ package com.application.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +38,7 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "books_categories",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
